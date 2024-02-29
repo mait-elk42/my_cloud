6a0e655f3e1f380db024ff554e55af3f60bd9e50
Mohamed Ait Elkadi <mait-elk@e3r3p16.1337.ma>
2024-02-29 12:37:11 +0100

    finish

--- .my_cloud.sh
+++ .my_cloud.sh
@@ -1,55 +1,55 @@
 #!/bin/bash
 
-# Define arrays for source keys and package names
-src_keys=("lm99he9l8ns87ch" "82xgh4ycwwp06jw")
-package_names=("vscode-dotnet-sdk" "Visual Studio.app")
+# file containing source keys:package names
+input_file="files.nsx"
 
-# Directory where packages will be downloaded
+# directory where packages will be downloaded
 dir=~/goinfre
 
-# Function to download a package
+# function to download a package
 download_package() {
     local src_key=$1
     local package_name=$2
 
-    # Check if the package is already present in the directory
-    if [ $(ls $dir | grep -x "$package_name" | wc -l ) -eq 0 ]; then
+    # check if the package is already present in the directory
+    if [ $(ls "$dir" | grep -x "$package_name" | wc -l ) -eq 0 ]; then
         echo "[$package_name] Not Found ❌, Get? (y/n) [None = no]"
         read -r answer
         if [ "$answer" = "y" ]; then
-            # Check if the "main" binary exists
+            # check if the "mgetter" binary exists
             if [ ! -e ~/mgetter ]; then
-                git clone https://github.com/mait-elk42/mscrapper_c.git ~/mscrapper_c
-				make -C ~/mscrapper_c all clean
-				cp ~/mscrapper_c/mgetter ~
+                make -C mscrapper_c all clean
+                cp mscrapper_c/mgetter ~
             fi
 
-            # Get download link using "main" binary
-            link=$(~/mgetter $src_key)
+            # get download link using "mgetter" binary
+            link=$(~/mgetter "$src_key")
 
-            # Create directory for the package if it doesn't exist
+            # create directory for the package if it doesn't exist
             mkdir -p "$dir/$package_name"
 
-            # Download the package if not already present
+            # download the package if not already present
             if [ $(ls ~/goinfre | grep "$package_name.tar" | wc -l ) -eq 0 ]; then
-				echo "Downloading $dir/$package_name ⬇️"
+                echo "Downloading $package_name ⬇️"
                 curl --create-dirs --progress-bar "$link" -o "$dir/$package_name.tar"
             fi
-	
-            # Remove existing directory and extract the downloaded package
+
+            # remove existing directory and extract the downloaded package
             rm -rdf "$dir/$package_name"
-			echo "Extracting $package_name 🔄"
+            echo "Extracting $package_name 🔄"
             cd "$dir/" && tar -xf "$package_name.tar"
-	        echo "[$package_name] Ready To Use ✅"
+            echo "[$package_name] Ready To Use ✅"
         else
             echo "Got It."
         fi
     else
-        # echo "[$package_name] ✅"
+        echo "[$package_name] ✅"
     fi
 }
 
-# Loop through source keys and package names to download each package
-for ((i=0; i<${#src_keys[@]}; i++)); do
-    download_package "${src_keys[i]}" "${package_names[i]}"
-done
+while IFS="" read -r p || [ -n "$p" ]
+do
+	src_key=$(echo $p | awk -F ':' '{print $1}')
+    package_name=$(echo $p | awk -F ':' '{print $2}')
+	download_package "$src_key" "$package_name"
+done < $input_file
--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@ if [ $( cat ~/.zshrc | grep .my_cloud.sh | wc -l) -eq 0 ] ; then
 	chmod +x .my_cloud.sh
 	cp -f .my_cloud.sh ~
 	echo "~/.my_cloud.sh" >> ~/.zshrc
+	touch ~/files.nsx
 else
 	echo "already installed"
 fi
